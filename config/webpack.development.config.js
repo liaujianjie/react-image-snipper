c01e550eb93a3e77e0cd3d3a994bf66b6b87fbee
Liau Jian Jie <liaujianjie@gmail.com>
2018-06-29 22:31:46 -0700

    Fix webpack config

--- config/webpack.development.config.js
+++ config/webpack.development.config.js
@@ -1,45 +1,34 @@
 const path = require('path');
 
-module.exports = (baseConfig, env, defaultConfig) => {
-  // defaultConfig.module.rules.push({
-  //   test: /\.(ts|tsx)$/,
-  //   include: path.resolve(__dirname, '../src'),
-  //   loader: require.resolve('ts-loader'),
-  // });
-
-  defaultConfig.module.rules.push(
-    {
-      test: /\.(jpe?g|png|gif|svg)$/,
-      include: path.resolve(__dirname, '../stories'),
-      loaders: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        include: path.resolve(__dirname, '../stories'),
+        loaders: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
           },
-        },
-      ],
-    },
-    {
-      test: /\.css$/,
-      include: path.resolve(__dirname, '../src'),
-      loaders: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            modules: true,
-            localIdentName: '[name]__[local]___[hash:base64:5]',
-            importLoaders: 1,
+        ],
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
           },
-        },
-        'postcss-loader',
-      ],
-    }
-  );
-
-  // defaultConfig.resolve.extensions.push('.ts', '.tsx');
-  // defaultConfig.resolve.extensions.push('.jpg');
-
-  return defaultConfig;
+        ],
+      },
+    ],
+  },
 };
